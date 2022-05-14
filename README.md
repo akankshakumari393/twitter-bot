82dbc4abdf719390d101af7ecfd2da9432658cc3
Akanksha Kumari <akankshakumari393@gmail.com>
2022-05-15 00:23:44 +0530

    Update the README, build script to use .env as argument

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # twitter-bot
 
-./build/build.sh
+Add an `.env` file with below keys (These keys would be generated from twitter developer account):
+
+```
+CONSUMER_KEY=""
+CONSUMER_SECRET=""
+ACCESS_TOKEN=""
+ACCESS_TOKEN_SECRET=""
+```
+
+to run the program use build script `./build/build.sh {path_to_.env_file}`
+
+```
+./build/build.sh ./.env
+```
 
 ![](images/tweet.png)
--- build/build.sh
+++ build/build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-set -a
-. ./.env
-set +a
+help(){
+    echo "Please provide one argument which points to relative path of .env file "
+    echo "For Example ./build.sh ./.env"
+    exit 1
+}
 
-go build
+if [ $# -ne 1 ] 
+then
+   help
+fi
+
+set -a # export all variables created next
+. $1
+set +a # stop exporting
+
+go build # build the program
+
 ./twitter-bot
